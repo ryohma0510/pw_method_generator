ee4d9e08abba57f4705002c72473586ba7143d4b
川原遼馬 <akahirefriend2008@yahoo.com>
2018-01-31 00:14:47 +0900

    make form for password component condition

--- app/assets/javascripts/passwords.js
+++ app/assets/javascripts/passwords.js
@@ -3,6 +3,7 @@ $(document).on('turbolinks:load', function() {
     $('.jsDeleteRow').on('dblclick', function () {
       $(this).remove();
       reNamingPwCompTable();
+      reNamingPwCompConditionTable();
     });
   }
 
@@ -24,15 +25,38 @@ $(document).on('turbolinks:load', function() {
     })
   }
 
+  var reNamingPwCompConditionTable = function() {
+    var pwCompConditions = $('.jsPwCompCondition')
+    var pwCompConditionNums = $('.jsPwCompConditionNum')
+
+    $.each(pwCompConditions, function(index, pwCompCondition) {
+      $(pwCompCondition).attr('name', 'pwCompCondition' + (index + 1));
+    });
+
+    $.each(pwCompConditionNums, function(index, pwCompConditionNum) {
+      $(pwCompConditionNum).attr('name', 'pwCompConditionNum' + (index + 1));
+    });
+  }
+
+  addDeleteRowEvent();
+
   $('.jsAddPwComp').on('click', function(e) {
     e.preventDefault();
     var count = $('.jsPwCompNumbering').length + 1;
-    var tableDataNumberHTML = '<td class="jsPwCompNumbering jsDeleteRow">' + count +'</td>';
+    var tableDataNumberHTML = '<td class="jsPwCompNumbering">' + count +'</td>';
     var tableDataPwCompLabelHTML = '<td><input type="text" name="pwCompLabel'+ count + '" value="" class="form-control jsPwCompLabel"></td>';
     var tableDataPwCompValueHTML = '<td><input type="text" name="pwCompValue'+ count + '" value="" class="form-control jsPwCompValue"></td>';
-    $('.jsNewPwCompForm').before('<tr>' + tableDataNumberHTML + tableDataPwCompLabelHTML + tableDataPwCompValueHTML + '</tr>');
+    $('.jsNewPwCompForm').before('<tr class="jsDeleteRow">' + tableDataNumberHTML + tableDataPwCompLabelHTML + tableDataPwCompValueHTML + '</tr>');
     addDeleteRowEvent();
   });
 
-  addDeleteRowEvent();
+  $('.jsAddPwCompCondition').on('click', function(e) {
+    e.preventDefault();
+    var count = $('.jsPwCompCondition').length + 1
+    var tableDataPwCompConditionHTML = '<td><input type="text" name="pwCompCondition'+ count + '" value="" class="form-control jsPwCompCondition"></td>';
+    var tableDataPwCompConditionNumHTML = '<td><input type="text" name="pwCompConditionNum'+ count + '" value="" class="form-control jsPwCompConditionNum"></td>';
+    $('.jsNewPwCompConditionForm').before('<tr class="jsDeleteRow">' + tableDataPwCompConditionHTML + tableDataPwCompConditionNumHTML + '</tr>');
+    addDeleteRowEvent();
+  })
+
 });
