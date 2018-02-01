7c83756bed41cbb6ccf2063a83cab69d93e563e2
川原遼馬 <akahirefriend2008@yahoo.com>
2018-02-01 19:58:28 +0900

    change name attributes

--- app/assets/javascripts/passwords.js
+++ app/assets/javascripts/passwords.js
@@ -19,11 +19,11 @@ $(document).on('turbolinks:load', function() {
     })
 
     $.each(pwCompLabels, function(index, pwCompLabel) {
-      $(pwCompLabel).attr('name', 'pwCompLabel' + (index + 1));
+      $(pwCompLabel).attr('name', 'pw_comp[' + (index + 1) + '][label]');
     });
 
     $.each(pwCompValues, function(index, pwCompValue) {
-      $(pwCompValue).attr('name', 'pwCompValue' + (index + 1));
+      $(pwCompValue).attr('name', 'pw_comp[' + (index + 1) + '][value]');
     })
   }
 
@@ -32,11 +32,11 @@ $(document).on('turbolinks:load', function() {
     var pwCompConditionNums = $('.jsPwCompConditionNum')
 
     $.each(pwCompConditions, function(index, pwCompCondition) {
-      $(pwCompCondition).attr('name', 'pwCompCondition' + (index + 1));
+      $(pwCompCondition).attr('name', 'pw_comp_condition[' + (index + 1) + '][label]');
     });
 
     $.each(pwCompConditionNums, function(index, pwCompConditionNum) {
-      $(pwCompConditionNum).attr('name', 'pwCompConditionNum' + (index + 1));
+      $(pwCompConditionNum).attr('name', 'pw_comp_condition[' + (index + 1) + '][value]');
     });
   }
 
@@ -49,7 +49,7 @@ $(document).on('turbolinks:load', function() {
     });
 
     $.each(pwMethods, function(index, pwMethod) {
-      $(pwMethod).attr('name', 'pwMethod' + (index + 1));
+      $(pwMethod).attr('name', 'pw_method[' + (index + 1) + ']');
     });
   }
 
@@ -58,11 +58,11 @@ $(document).on('turbolinks:load', function() {
     var pwMethodConditionValues = $('.jsPwMethodConditionValue');
 
     $.each(pwMethodConditions, function(index, pwMethodCondition) {
-      $(pwMethodCondition).attr('name', 'pwMethodCondition' + (index + 1));
+      $(pwMethodCondition).attr('name', 'pw_method_condition[' + (index + 1) + '][label]');
     })
 
     $.each(pwMethodConditionValues, function(index, pwMethodConditionValue) {
-      $(pwMethodConditionValue).attr('name', 'pwMethodConditionValue' + (index + 1));
+      $(pwMethodConditionValue).attr('name', 'pw_method_condition[' + (index + 1) + '][value]');
     })
   }
 
@@ -72,8 +72,8 @@ $(document).on('turbolinks:load', function() {
     e.preventDefault();
     var count = $('.jsPwCompNumbering').length + 1;
     var tableDataNumberHTML = '<td class="jsPwCompNumbering">' + count +'</td>';
-    var tableDataPwCompLabelHTML = '<td><input type="text" name="pwCompLabel'+ count + '" value="" class="form-control jsPwCompLabel"></td>';
-    var tableDataPwCompValueHTML = '<td><input type="text" name="pwCompValue'+ count + '" value="" class="form-control jsPwCompValue"></td>';
+    var tableDataPwCompLabelHTML = '<td><input type="text" name="pw_comp[' + count + '][label]" value="" class="form-control jsPwCompLabel"></td>';
+    var tableDataPwCompValueHTML = '<td><input type="text" name="pw_comp[' + count + '][value]" value="" class="form-control jsPwCompValue"></td>';
     $('.jsNewPwCompForm').before('<tr class="jsDeleteRow">' + tableDataNumberHTML + tableDataPwCompLabelHTML + tableDataPwCompValueHTML + '</tr>');
     addDeleteRowEvent();
   });
@@ -81,17 +81,17 @@ $(document).on('turbolinks:load', function() {
   $('.jsAddPwCompCondition').on('click', function(e) {
     e.preventDefault();
     var count = $('.jsPwCompCondition').length + 1;
-    var tableDataPwCompConditionHTML = '<td><input type="text" name="pwCompCondition'+ count + '" value="" class="form-control jsPwCompCondition"></td>';
-    var tableDataPwCompConditionNumHTML = '<td><input type="text" name="pwCompConditionNum'+ count + '" value="" class="form-control jsPwCompConditionNum"></td>';
+    var tableDataPwCompConditionHTML = '<td><input type="text" name="pw_comp_condition['+ count + '][label]" value="" class="form-control jsPwCompCondition"></td>';
+    var tableDataPwCompConditionNumHTML = '<td><input type="text" name="pw_comp_condition['+ count + '][value]" value="" class="form-control jsPwCompConditionNum"></td>';
     $('.jsNewPwCompConditionForm').before('<tr class="jsDeleteRow">' + tableDataPwCompConditionHTML + tableDataPwCompConditionNumHTML + '</tr>');
     addDeleteRowEvent();
-  })
+  });
 
   $('.jsAddPwMethod').on('click', function(e) {
     e.preventDefault();
     var count = $('.jsPwMethodNumbering').length + 1;
     var tableDataPwMethodNumberHTML = '<td class="jsPwMethodNumbering">' + count +'</td>';
-    var tableDataPwMethodHTML = '<td><input type="text" name="pwMethod'+ count + '" value="" class="form-control jsPwMethod"></td>';
+    var tableDataPwMethodHTML = '<td><input type="text" name="pw_method['+ count + ']" value="" class="form-control jsPwMethod"></td>';
     $('.jsNewPwMethodForm').before('<tr class="jsDeleteRow">' + tableDataPwMethodNumberHTML + tableDataPwMethodHTML + '</tr>');
     addDeleteRowEvent();
   });
@@ -99,8 +99,8 @@ $(document).on('turbolinks:load', function() {
   $('.jsAddPwMethodCondition').on('click', function(e) {
     e.preventDefault();
     var count = $('.jsPwMethodCondition').length + 1;
-    var tableDataPwMethodCondition = '<td><input type="text" name="pwMethodCondition'+ count + '" value="" class="form-control jsPwMethodCondition"></td>';
-    var tableDataPwMethodConditionValue = '<td><input type="text" name="pwMethodConditionValue'+ count + '" value="" class="form-control jsPwMethodConditionValue"></td>';
+    var tableDataPwMethodCondition = '<td><input type="text" name="pw_method_condition['+ count + '][label]" value="" class="form-control jsPwMethodCondition"></td>';
+    var tableDataPwMethodConditionValue = '<td><input type="text" name="pw_method_condition['+ count + '][value]" value="" class="form-control jsPwMethodConditionValue"></td>';
     $('.jsNewPwMethodConditionForm').before('<tr class="jsDeleteRow">' + tableDataPwMethodCondition + tableDataPwMethodConditionValue + '</tr>');
     addDeleteRowEvent();
   });
