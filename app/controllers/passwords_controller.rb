a1377387e0fc670589b822a1fa1e0f117766477a
川原遼馬 <akahirefriend2008@yahoo.com>
2018-02-03 13:42:30 +0900

    add pdf generate function

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,10 +4,31 @@ class PasswordsController < ApplicationController
   end
 
   def pdf
-    binding.pry
+    @pw_comp = pw_comp
+    @pw_comp_condition = pw_comp_condition
+    @pw_method = pw_method
+    @pw_method_condition = pw_method_condition
     render pdf: 'pdf',
            layout: 'pdflayouts.html.slim',
            template: '/passwords/pdf.html.slim',
            encording: 'UTF-8'
-  end         
+  end
+
+  private
+
+  def pw_comp
+    return params.require(:pw_comp).permit!
+  end
+
+  def pw_comp_condition
+    return params.require(:pw_comp_condition).permit!
+  end
+
+  def pw_method
+    return params.require(:pw_method).permit!
+  end
+
+  def pw_method_condition
+    return params.require(:pw_method_condition).permit!
+  end
 end
